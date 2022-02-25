f3407a0e582592de29055456665c39d0629b7d0b
ramuchandru <ram@thescube.com>
2022-02-25 12:35:42 +0530

    update

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "8DSh8L49iogQd1jNUtFqPcNZWmkOuRNF7dT4ckBBQYQ",
+      "spaceId": "f8prwnjrws8j"
+    }
+  }, "gatsby-plugin-sass"]
+};
