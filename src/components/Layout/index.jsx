0fc6bcc5e7c16e8e40a74b0d4d7464ea100f2ba9
Sugandar <sugandar@thescube.com>
2022-02-26 13:38:16 +0530

    reset css added and pages file ext updated

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-
 import { Header } from "components";
+import "styles/global.css";
 
 export const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
